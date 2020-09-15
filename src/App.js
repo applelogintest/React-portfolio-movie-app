3aec682571a4abc29a6160edd5e93d3ceae7c94e
HakMin <kimys260@naver.com>
2020-09-15 19:07:48 +0900

    open API를 통한 영화 리스트 가져오기 구현중

--- src/App.js
+++ src/App.js
@@ -3,12 +3,56 @@ import Movie from './js/Movie';
 import './App.css';
 
 class App extends Component {
-  render() {
+  state = {};
+
+  _callAPI = async () => {
+    const ratingPromise = this._getMoviesPromise('rating');
+    const downloadCountPromise = this._getMoviesPromise('download_count');
+    const likeCountPromise = this._getMoviesPromise('like_count');
+
+    const ratingMovies = await ratingPromise;
+    const downloadCountMovies = await downloadCountPromise;
+    const likeCountMovies = await likeCountPromise;
+
+    this.setState({
+      ratingMovies,
+      downloadCountMovies,
+      likeCountMovies,
+    });
+  };
+
+  _renderMovies = () => {
     return (
-      <div className="App">
-        <Movie text="hello"></Movie>
+      <Movie
+        ratingMovies={this.state.ratingMovies}
+        downloadCountMovies={this.state.downloadCountMovies}
+        likeCountMovies={this.state.likeCountMovies}
+      ></Movie>
+    );
+  };
+
+  render() {
+    const { movies } = this.state;
+    console.log(movies);
+    console.log(this.state);
+    return (
+      <div className={this.state ? 'App' : 'App--loading'}>
+        {this.state ? this._renderMovies() : 'loading'}
       </div>
     );
+  }
+
+  componentDidMount() {
+    this._callAPI();
+  }
+
+  _getMoviesPromise(search) {
+    return new Promise((resolve, reject) => {
+      fetch(`https://yts.mx/api/v2/list_movies.json?sort_by=${search}`)
+        .then((response) => response.json())
+        .then((json) => resolve(json.data.movies))
+        .catch((error) => console.log(error));
+    });
   }
 }
 
