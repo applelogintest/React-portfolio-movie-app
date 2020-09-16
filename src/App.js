230f4a62bab1904121f70ba0b977de60a5be2335
HakMin <kimys260@naver.com>
2020-09-16 18:36:27 +0900

    state 값 안받지는것 해결

--- src/App.js
+++ src/App.js
@@ -1,59 +1,49 @@
 import React, { Component } from 'react';
-import Movie from './js/Movie';
+import Movie from './components/Movie';
 import './App.css';
 
 class App extends Component {
   state = {};
 
-  _callAPI = async () => {
-    const ratingPromise = this._getMoviesPromise('rating');
-    const downloadCountPromise = this._getMoviesPromise('download_count');
-    const likeCountPromise = this._getMoviesPromise('like_count');
-
-    const ratingMovies = await ratingPromise;
-    const downloadCountMovies = await downloadCountPromise;
-    const likeCountMovies = await likeCountPromise;
-
-    this.setState({
-      ratingMovies,
-      downloadCountMovies,
-      likeCountMovies,
-    });
-  };
-
-  _renderMovies = () => {
-    return (
-      <Movie
-        ratingMovies={this.state.ratingMovies}
-        downloadCountMovies={this.state.downloadCountMovies}
-        likeCountMovies={this.state.likeCountMovies}
-      ></Movie>
-    );
-  };
-
   render() {
     const { movies } = this.state;
-    console.log(movies);
-    console.log(this.state);
     return (
-      <div className={this.state ? 'App' : 'App--loading'}>
-        {this.state ? this._renderMovies() : 'loading'}
+      <div className={movies ? 'App' : 'App--loading'}>
+        {movies ? this._renderMovies() : 'loading'}
       </div>
     );
   }
 
   componentDidMount() {
-    this._callAPI();
+    this._getMovies();
   }
 
-  _getMoviesPromise(search) {
+  _renderMovies = () => {
+    return <Movie movies={this.state.movies} key={this.state.movies}></Movie>;
+  };
+
+  _getMovies = async () => {
+    const ratingPromise = this._getMoviePromise('rating');
+    const downloadPromise = this._getMoviePromise('download_count');
+    const likeCountPromise = this._getMoviePromise('like_count');
+
+    const ratingMovies = await ratingPromise;
+    const downloadCountMovies = await downloadPromise;
+    const likeCountMovies = await likeCountPromise;
+
+    this.setState({
+      movies: [ratingMovies, downloadCountMovies, likeCountMovies],
+    });
+  };
+
+  _getMoviePromise = (search) => {
     return new Promise((resolve, reject) => {
       fetch(`https://yts.mx/api/v2/list_movies.json?sort_by=${search}`)
         .then((response) => response.json())
         .then((json) => resolve(json.data.movies))
-        .catch((error) => console.log(error));
+        .catch((err) => console.log(err));
     });
-  }
+  };
 }
 
 export default App;
