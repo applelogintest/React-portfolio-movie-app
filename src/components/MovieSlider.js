7ef9d3e448d50d9a1128c55f8cf53343b4a789fe
HakMin <kimys260@naver.com>
2020-09-17 18:28:12 +0900

    슬라이더를 위한 html 구조 변경, 데이터에 맞는 슬라이더 아이템 출력 (슬라이드 기능 미구현)

--- src/components/MovieSlider.js
+++ src/components/MovieSlider.js
@@ -1,23 +1,78 @@
 import React from 'react';
 import '../css/MovieSlider.css';
 
-function MovieSlider() {
+function MovieSliderTitle({ type }) {
+  let result = '';
+  switch (type) {
+    case 'rating':
+      result = '현재 평점이 가장 좋은 영화!';
+      break;
+    case 'download':
+      result = '가장 많이 다운로드수가 많은 영화!';
+      break;
+    case 'like':
+      result = '사람들이 좋아하는 명작 영화!';
+      break;
+    default:
+      return;
+  }
+
+  return <h1 className="Movie__recommend__title">{result}</h1>;
+}
+
+function MovieSliderItem({ movies }) {
+  const title = movies
+    .slice(0, 2)
+    .map((movie) => {
+      return movie.title;
+    })
+    .join(', ');
+
   return (
-    <section className="Movie__slider">
+    <span className="Movie__titles">
+      {title} 등
+      <br /> 지금 가장 많이 보는 작품!
+    </span>
+  );
+}
+
+function getMovieSliderBg(urlStr) {
+  const style = {
+    background: `url('${urlStr}'),
+    no-repeat linear-gradient(to bottom, white, rgb(20, 21, 23, 0.4))`,
+    backgroundSize: '100% 100%',
+  };
+
+  return style;
+}
+
+function MovieSlider({ movies, bg }) {
+  const result = [];
+  for (let key in movies) {
+    result.push(
+      <section
+        className="Movie__slider__container"
+        style={getMovieSliderBg(bg[key])}
+        key={key}
+      >
+        <article className="Movie__sliderItem" key={key}>
+          <MovieSliderTitle type={key} />
+          <MovieSliderItem movies={movies[key]} />
+          <button className="Movie__list__btn">
+            <i className="fas fa-list"></i>
+            목록 보기
+          </button>
+        </article>
+      </section>
+    );
+  }
+
+  return (
+    <div className="Movie__slider">
       <i className="fas fa-chevron-left fa-3x" />
-      <article className="Movie__sliderItem">
-        <h1 className="Movie__recommend__title">왓챠 최고 인기작</h1>
-        <span className="Movie__titles">
-          나쁜 녀석들: 더 무비, 엑시트 등
-          <br /> 지금 가장 많이 보는 작품!
-        </span>
-        <button className="Movie__list__btn">
-          <i className="fas fa-list"></i>
-          목록 보기
-        </button>
-      </article>
+      {result}
       <i className="fas fa-chevron-right fa-3x"></i>
-    </section>
+    </div>
   );
 }
 
