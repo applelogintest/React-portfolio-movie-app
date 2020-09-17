90dfaf0eb6e4620a15e722cb175d43ec56e99f06
HakMin <kimys260@naver.com>
2020-09-17 18:26:19 +0900

    데이터 title에 따른 제목 출력, 데이터를 객체 변경후 데이터의 맞춰서 다시 코딩

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,66 +1,68 @@
 import React from 'react';
 import '../css/MovieList.css';
 
-function MovieBoxTitle({ movies }) {
-  let title = '';
-  switch (movies) {
-    case 'ratingMovies':
-      title = '평점순 인기 영화 리스트';
+function MovieBoxTitle({ title }) {
+  let result = '';
+  switch (title) {
+    case 'rating':
+      result = '평점순 인기 영화 리스트';
       break;
-    case 'downloadCountMovies':
-      title = '다운로드 회수 높은 영화 리스트';
+    case 'download':
+      result = '다운로드 회수 높은 영화 리스트';
       break;
-    case 'likeCountMovies':
-      title = '관객이 좋은 평가를 준 영화 리스트';
+    case 'like':
+      result = '관객이 좋은 평가를 준 영화 리스트';
       break;
     default:
       return;
   }
 
-  return `<span className="Movie__box__title">${title}</span>`;
+  return <span className="Movie__box__title">{result}</span>;
 }
 
-function MovieBoxhtml({ movies, key }) {
-  const result = movies.map((movieList) => {
-    return (
-      <section className="Movie__box">
+function MovieBoxhtml({ movies }) {
+  const result = [];
+  for (let key in movies) {
+    result.push(
+      <section className="Movie__box" key={key}>
         <div className="Movie__box__arrow left">
           <i className="fas fa-chevron-left fa-3x"></i>
         </div>
-        <MovieListHtml movieList={movieList} key={movieList.id}></MovieListHtml>
+        <article className="Movie__box__list">
+          <MovieBoxTitle title={key}></MovieBoxTitle>
+          <div className="Movie__box__items">
+            <MovieListHtml movieList={movies[key]}></MovieListHtml>
+          </div>
+        </article>
         <div className="Movie__box__arrow right">
           <i className="fas fa-chevron-right fa-3x"></i>
         </div>
       </section>
     );
-  });
+  }
 
   return result;
 }
 
-function MovieListHtml({ movieList, key }) {
+function MovieListHtml({ movieList }) {
   const result = movieList.map((movie) => {
     return (
-      <article className="Movie__box__list">
-        <div className="Movie__box__items">
-          <div className="Movie__box__item">
-            <img
-              className="Movie__box__poster"
-              src={movie.medium_cover_image}
-              alt={movie.title}
-            />
-            <span className="Movie__box__movie">{movie.title}</span>
-          </div>
-        </div>
-      </article>
+      <div className="Movie__box__item" key={movie.id}>
+        <img
+          className="Movie__box__poster"
+          src={movie.medium_cover_image}
+          alt={movie.title}
+        />
+        <span className="Movie__box__movie">{movie.title}</span>
+      </div>
     );
   });
 
   return result;
 }
 
-function MovieList({ movies, key }) {
-  return <MovieBoxhtml movies={movies} key={key}></MovieBoxhtml>;
+function MovieList({ movies }) {
+  return <MovieBoxhtml movies={movies}></MovieBoxhtml>;
 }
 
 export default MovieList;
