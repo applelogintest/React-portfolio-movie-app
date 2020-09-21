d2316831096d33e69404a29ba05432c0bf6d95e0
HakMin <kimys260@naver.com>
2020-09-21 19:21:50 +0900

    슬라이더 구조로 진행하다 width의 문제가 발생하여 watcha 처럼 클릭시 이미지 텍스트를 변경하는 방식으로 바꿈

--- src/components/MovieSlider.js
+++ src/components/MovieSlider.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../css/MovieSlider.css';
 
-function MovieSliderTitle({ type }) {
+function getMovieSliderTitle(type) {
   let result = '';
   switch (type) {
     case 'rating':
@@ -17,10 +17,10 @@ function MovieSliderTitle({ type }) {
       return;
   }
 
-  return <h1 className="Movie__recommend__title">{result}</h1>;
+  return result;
 }
 
-function MovieSliderItem({ movies }) {
+function getMovieSliderItem(movies) {
   const title = movies
     .slice(0, 2)
     .map((movie) => {
@@ -28,80 +28,117 @@ function MovieSliderItem({ movies }) {
     })
     .join(', ');
 
-  return (
-    <span className="Movie__titles">
-      {title} 등
-      <br /> 지금 가장 많이 보는 작품!
-    </span>
-  );
+  return title;
 }
 
 function MovieSlider({ movies, bg }) {
-  const result = [];
-  for (let key in movies) {
-    result.push(
-      <div
-        className="Movie__slider__slide"
-        style={getMovieSliderBg(bg[key])}
-        key={key}
-      >
-        <article className="Movie__sliderItem" key={key}>
-          <MovieSliderTitle type={key} />
-          <MovieSliderItem movies={movies[key]} />
+  return (
+    <div
+      className="Movie__slider__container"
+      onClick={(event) => movieSliderClick(event, movies, bg)}
+    >
+      <section className="Movie__slider" style={getMovieSliderStyle(bg.rating)}>
+        <article className="Movie__sliderItem">
+          <h1 className="Movie__recommend__title">
+            {getMovieSliderTitle('rating')}
+          </h1>
+          <span className="Movie__titles fade-in">
+            {getMovieSliderItem(movies.rating)} 등 <br /> 지금 가장 많이 보는
+            작품!
+          </span>
           <button className="Movie__list__btn">
             <i className="fas fa-list"></i>
             목록 보기
           </button>
         </article>
-      </div>
-    );
-  }
-
-  return (
-    <div
-      className="Movie__slider__container"
-      onClick={(event) => movieSliderClick(event)}
-    >
-      <section className="Movie__slider__slides">{result}</section>
+      </section>
       <i className="fas fa-chevron-left fa-3x" />
       <i className="fas fa-chevron-right fa-3x" />
     </div>
   );
 }
 
-function getMovieSliderBg(urlStr) {
+function getMovieSliderStyle(urlStr, key) {
   const style = {
     background: `url('${urlStr}'),
     no-repeat linear-gradient(to bottom, white, rgb(20, 21, 23, 0.4))`,
     backgroundSize: '100% 100%',
+    title: getMovieSliderTitle(`${key}`),
   };
 
   return style;
 }
 
-function movieSliderClick(event) {
+function movieSliderClick(event, movies, bg) {
   const target = event.target.className;
 
   switch (target) {
     case 'fas fa-chevron-left fa-3x':
     case 'fas fa-chevron-right fa-3x':
-      arrow(target);
+      arrow(target, movies, bg);
       break;
     default:
       break;
   }
 }
 
-function arrow(direction) {
+function arrow(direction, movies, bg) {
   const regExp = /.fa-chevron-left/;
-  const movieSliders = document.querySelector('.Movie__slider__slides');
+  const bgArr = Object.values(bg);
 
   if (regExp.test(direction)) {
-    console.log('left');
+    chageSlider(bgArr, movies, 'left');
   } else {
-    console.log('right');
-    movieSliders.style.transform = 'translateX(-200%)';
+    chageSlider(bgArr, movies, 'right');
   }
+}
+
+function chageSlider(bgArr, movies, direction) {
+  const movieSlider = document.querySelector('.Movie__slider');
+  const sliderTitle = document.querySelector('.Movie__recommend__title');
+  const sliderItem = document.querySelector('.Movie__titles');
+
+  const movieKeys = Object.keys(movies);
+  const movieObj = Object.values(movies);
+
+  const bgStr = getBacgroundUrlStr(movieSlider.style.background);
+  const currentIndex = bgArr.findIndex((value) => {
+    return value === bgStr;
+  });
+
+  let index = 0;
+  index = getIndex(currentIndex, bgArr.length, direction);
+
+  const stlye = getMovieSliderStyle(bgArr[index]);
+  movieSlider.style.background = `${stlye.background}`;
+  movieSlider.style.backgroundSize = `${stlye.backgroundSize}`;
+  sliderTitle.textContent = getMovieSliderTitle(movieKeys[index]);
+  sliderItem.innerHTML =
+    //
+    `${getMovieSliderItem(
+      movieObj[index]
+    )} 등 <br /> 지금 가장 많이 보는 작품! `;
+
+  movieSlider.classList.add('fade-in');
+}
+
+function getIndex(index, length, direction) {
+  let result = 0;
+  if (direction === 'left') {
+    result = index - 1;
+    return result < 0 ? length - 1 : result;
+  } else {
+    result = index + 1;
+    return result === length ? 0 : result;
+  }
+}
+
+function getBacgroundUrlStr(bgStr) {
+  const result = bgStr.substr(
+    bgStr.indexOf('"') + 1,
+    bgStr.lastIndexOf('"') - 5
+  );
+  return result;
 }
 
 export default MovieSlider;
