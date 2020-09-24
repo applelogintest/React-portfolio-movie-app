f3e8d7b7e90526fb080ba918acd950bac9a14830
HakMin <kimys260@naver.com>
2020-09-24 18:29:40 +0900

    검색 input 태그 추가 및 클릭시 나타나도록 변경

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../css/Header.css';
 
-function Header() {
+function Header({ handleInputSearchChange }) {
   return (
     <header className="Movie__header">
       <nav className="Movie__nav">
@@ -11,14 +11,42 @@ function Header() {
         </ul>
         <ul className="nav__items">
           <li className="nav__movie__search">
-            <i className="fas fa-search "></i>
-            검색
+            <input
+              className="nav__movie__input"
+              type="text"
+              placeholder="제목 검색"
+              visibility="hidden"
+              onChange={(e) => searchOnChage(e, handleInputSearchChange)}
+            ></input>
+            <i className="fas fa-search " onClick={clickSearch}>
+              <span>검색</span>
+            </i>
           </li>
           <li className="nav__login">로그인</li>
         </ul>
       </nav>
     </header>
   );
+}
+
+function searchOnChage(e, handleInputSearchChange) {
+  const value = e.target.value;
+
+  handleInputSearchChange(value);
+}
+
+function clickSearch() {
+  const input = document.querySelector('.nav__movie__input');
+
+  const inputVisible = input.style.visibility;
+
+  if (inputVisible === 'visible') {
+    input.style.width = '0px';
+    input.style.visibility = 'hidden';
+  } else {
+    input.style.width = '180px';
+    input.style.visibility = 'visible';
+  }
 }
 
 export default Header;
