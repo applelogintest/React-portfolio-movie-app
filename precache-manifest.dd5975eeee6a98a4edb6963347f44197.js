df791f64fc0bca8cadd09147f3e0527b273e463a
HakMin <kimys260@naver.com>
2020-09-28 17:17:41 +0900

    Updates

---
+++ precache-manifest.dd5975eeee6a98a4edb6963347f44197.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "530e68249c005519b164a7708f944fa1",
+    "url": "/React-portfolio-movie-app/index.html"
+  },
+  {
+    "revision": "b45585c47db60d935a1f",
+    "url": "/React-portfolio-movie-app/static/css/main.28db0c69.chunk.css"
+  },
+  {
+    "revision": "0bd5ae283f79b2d7c7f9",
+    "url": "/React-portfolio-movie-app/static/js/2.82ee2f15.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-portfolio-movie-app/static/js/2.82ee2f15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b45585c47db60d935a1f",
+    "url": "/React-portfolio-movie-app/static/js/main.1ecb09c0.chunk.js"
+  },
+  {
+    "revision": "0d541b40843ebd212c5d",
+    "url": "/React-portfolio-movie-app/static/js/runtime-main.c767408c.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/React-portfolio-movie-app/precache-manifest.dd5975eeee6a98a4edb6963347f44197.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-portfolio-movie-app/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
