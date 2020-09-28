710a9718d0b32753b60a16cc6f463dfa468f1fab
HakMin <kimys260@naver.com>
2020-09-28 17:30:12 +0900

    Footer HTML 구조 작성

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,37 @@ import React from 'react';
 import '../css/Footer.css';
 
 function Footer() {
-  return <footer className="Movie__footer">Movie__footer</footer>;
+  return (
+    <footer className="Movie__footer">
+      <div className="Movie__footer__container">
+        <div className="Footer__Title">
+          <span>Contact</span>
+        </div>
+        <div className="Footer__Email">
+          <span>Email</span>
+          <a href="mailto:kimhm260@gmail.com?subject=feedback">
+            <i class="far fa-envelope fa-2x" />
+          </a>
+        </div>
+        <div className="Footer__Github">
+          <span>Github</span>
+          <a
+            href="https://github.com/kimhm93/React-portfolio-movie-app"
+            target="_blink"
+          >
+            <i class="fab fa-github-alt fa-2x" />
+          </a>
+        </div>
+        <div className="Footer__Blog">
+          <span>Blog</span>
+          <a href="https://mongtak.tistory.com/" target="_blink">
+            <i class="fas fa-blog fa-2x" />
+          </a>
+        </div>
+        <div className="Footer__Info">KimHakMin Portfolio</div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
